3fead257b0a6a1d412affc6ffe8c0b980c761070
Joshua <yoonj200@gmail.com>
2022-01-23 12:30:05 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const name = "Joe";
 const height = 74;
-const message = `${name} is ${height} inches tall`;
+const message = `${name} is ${height} inches tall`; // Interpolation
 
 module.exports = {name, height, message};
+
+// Use console.log, then type "node index.js" into terminal
+console.log(message);
